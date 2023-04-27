645094f501ad8ad667e10c52bfff0f6f860c5590
takaaki2255 <red.ducati.mac2255@gmail.com>
2023-04-27 05:01:25 +0000

    php Laravel 11 課題用

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -5,9 +5,21 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>MyNews</title>
+        <title>Laravel</title>
     </head>
     <body>
-        <h1>initial commit</h1>
+        @extends('layouts.admin')
+        
+        @section('title', 'My プロフィール')
+        
+        @section('content')
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 mx-auto">
+                        <h2>My プロフィール</h2>
+                    </div>
+                </div>
+            </div>
+        @endsection
     </body>
 </html>
