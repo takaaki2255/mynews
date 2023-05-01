56a701b463005e0d34ccaeecbab89207e40321ea
takaaki2255 <red.ducati.mac2255@gmail.com>
2023-05-01 03:46:22 +0000

    Laravel15

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,11 @@ Route::controller(ProfileController::class)
     Route::post('profile/edit', 'update')->name('profile.update');
     
 });
+
+Route::controller(NewsController::class)->prefix('admin')
+    ->name('admin.')->middleware('auth')->group(function () {
+    Route::get('news/create', 'add')->name('news.add');
+    Route::post('news/create', 'create')->name('news.create');
+    Route::get('news', 'index')->name('news.index');
+});
+
